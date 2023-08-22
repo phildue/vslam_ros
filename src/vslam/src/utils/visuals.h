603a5af59c3760d97c3eca4c256c05568639e342
Philipp Dürnay <phild@protonmail.com>
2023-08-22 07:15:30 +0000

    add generic hstack

--- src/vslam/src/utils/visuals.h
+++ src/vslam/src/utils/visuals.h
@@ -3,6 +3,10 @@
 
 #include "core/Frame.h"
 #include "core/types.h"
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <tuple>
+
 namespace vslam {
 cv::Mat colorizedDepth(const cv::Mat &depth, double zMax = -1);
 cv::Mat blend(const cv::Mat &mat1, const cv::Mat &mat2, double weight1);
@@ -12,6 +16,22 @@ cv::Mat visualizeFramePair(Frame::ConstShPtr f0, Frame::ConstShPtr f1);
 namespace overlay {
 cv::Mat frames(Frame::VecConstShPtr frames);
 
-}
+template <class... Drawables> class Hstack {
+private:
+  std::tuple<Drawables...> _drawables;
+
+public:
+  Hstack(Drawables &&...drawables) :
+      _drawables(std::forward<Drawables>(drawables)...) {}
+
+  cv::Mat operator()() const {
+    std::vector<cv::Mat> mats;
+    std::apply([&](const auto &...drawables) { (mats.push_back(drawables.draw()), ...); }, _drawables);
+    cv::Mat mat;
+    cv::hconcat(mats, mat);
+    return mat;
+  }
+};
+}  // namespace overlay
 }  // namespace vslam
 #endif  // VSLAM_VISUALS_H__
