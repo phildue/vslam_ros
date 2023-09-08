ea4fdb1ec45903f3053fd04b97a38520ed17b4c9
Philipp Dürnay <phild@protonmail.com>
2023-09-08 07:51:45 +0000

    extend header

--- src/vslam/include/vslam/features.h
+++ src/vslam/include/vslam/features.h
@@ -17,5 +17,5 @@
 #define VSLAM_KEYPOINT_SELECTION_PACKAGE_H__
 #include "features/FeatureSelection.h"
 #include "features/keypoint_selection.h"
-
+#include "features/overlays.h"
 #endif  // VSLAM_KEYPOINT_SELECTION_PACKAGE_H__
